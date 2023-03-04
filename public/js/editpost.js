3e333f63fdad4033cd151a67df838875358887df
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-04 23:50:44 +0000

    fixed delete in editpost.js

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -1,50 +1,57 @@
-
+//front end request to edit a post 
 const updateFormHandler = async (event) => {
-    event.preventDefault();
-    if (event.target.hasAttribute("data-id")) {
-      const id = event.target.getAttribute("data-id");
-      const postContent = document.querySelector(".post-content").value.trim();
-      const titletUpdate = document.querySelector(".post-title").value.trim();
+  event.preventDefault();
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    // Grab title and post content
+    const postContent = document.querySelector(".post-content").value.trim();
+    const titletUpdate = document.querySelector(".post-title").value.trim();
 
-      const response = await fetch(`/api/posts/${id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-          post_title: titletUpdate,
-          post_content: postContent,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      if (response.ok) {
-        // replace url
-        document.location.replace("/profile");
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        post_title: titletUpdate,
+        post_content: postContent,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      // replace url
+      document.location.replace("/profile");
+    } else {
+      // console log an error message
     }
-  };
-}
+  }
+};
 
-  // Delete a post
-  const deletePost = async (event) => {
-    if (event.target.hasAttribute("data-id")) {
-      const postId = event.target.getAttribute("data-id");
-      const response = await fetch(`/api/posts/${postId}`, {
-        method: "DELETE",
-        body: JSON.stringify({
-          id: postId,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      if (response.ok) {
-        document.location.href = "/";
+// Delete a post
+const deletePost = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const postId = event.target.getAttribute("data-id");
+    console.log(postId)
+    const response = await fetch(`/api/posts/${postId}`, {
+      method: "DELETE",
+      body: JSON.stringify({
+        id: postId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      // redirect to dashboard
+      document.location.href = "/";
+    } else {
+      // console log an error message
+      console.log(response.statusText);
     }
-  };
-}
+  }
+};
 
+document.querySelector("#delete-btn").addEventListener("click", deletePost);
 
-  document.querySelector("#delete-btn").addEventListener("click", deletePost);  
-
-  document.querySelector("#update-post").addEventListener("click", updateFormHandler);
+document.querySelector("#update-post").addEventListener("click", updateFormHandler);
